6c46db66ea7cb2bbf01f5938fbc2b69b69cf8dbd
Chris Martinez <chrisumartinez@gmail.com>
2019-11-19 02:14:31 -0800

    Google Sign-In Authentication Implemented, TODO::Email Implementation, Construct Firebase Singleton Design

--- app/src/main/java/com/example/Elotero/Activities/MainActivity.java
+++ app/src/main/java/com/example/Elotero/Activities/MainActivity.java
@@ -2,13 +2,22 @@ package com.example.Elotero.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.Elotero.R;
 import com.firebase.ui.auth.AuthUI;
+
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 
 import java.util.Arrays;
 import java.util.List;
@@ -25,18 +34,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onStart(){
         super.onStart();
 
-
         List<AuthUI.IdpConfig> providers = Arrays.asList(
                 new AuthUI.IdpConfig.EmailBuilder().build(),
-                //new AuthUI.IdpConfig.FacebookBuilder().build(),
+             //   new AuthUI.IdpConfig.FacebookBuilder().build(),
                 new AuthUI.IdpConfig.GoogleBuilder().build());
 
 
 
         Button registerButton = (Button)findViewById(R.id.registerBtn);
+
         registerButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this, RegisterScreen.class);
                 startActivityForResult(
                         AuthUI.getInstance()
                                 .createSignInIntentBuilder()
@@ -45,5 +55,30 @@ public class MainActivity extends AppCompatActivity {
                         Globals.RC_SIGN_IN);
             }
         });
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == Globals.RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                // Successfully signed in
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                startActivity(new Intent(this, RegisterScreen.class));
+                //Continue Registration in Registration Screen::
+
+
+            } else {
+                // Sign in failed.
+                if(resultCode == Activity.RESULT_CANCELED){
+                    Toast.makeText(this, "Sign In Cancelled.", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+            }
+        }
     }
 }
